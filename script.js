3f4b900e8a8bc203d432088b4958902f4ba0592f
juan meisus <130049281+juanmeisus@users.noreply.github.com>
2023-10-07 22:54:13 +0000

    carajo

---
+++ script.js
@@ -0,0 +1,11 @@
+const who = ["The dog", "My grandma", "His turtle", "My bird"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "the keys", "the car"];
+const when = ["before the class", "right on time","when I finished","during my lunch", "while I was praying",];
+
+const boton = document.getElementById(`renovar`);
+boton.addEventListener(`click`, cambiarFrase);
+
+function cambiarFrase() {
+  console.log ("carajo");
+}
